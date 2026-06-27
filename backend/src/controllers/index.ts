945247c29f966f950d94703afb7702467590fd3e
anubhav-kayal <kayalanubhav14@gmail.com>
2026-06-27 06:05:12 +0530

    feat(backend): add admin controller for platform oversight and controller barrel exports

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,5 @@
+export { AuthController } from './authController';
+export { studentController } from './studentController';
+export { courierController } from './courierController';
+export { restaurantController } from './restaurantController';
+export { adminController } from './adminController';
